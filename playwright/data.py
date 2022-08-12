fac785758789cfffad64ceb8226a8a206c062057
shay dayan <shay.etl.dayan@gmail.com>
2022-08-12 03:18:29 +0300

    data.py was added

--- playwright/data.py
+++ playwright/data.py
@@ -1,3 +1,2 @@
 right_user = ["shay_dayan@gmail.com","123456"]
-wrong_users_list = [["shay_dayan@gmail.com","1234"],["shay_dayan@gmail.com","1234567"],["shay_dayan@gmail.co","12345"],["shay_dayan@gmail.com","1234567"]]
-
+wrong_users_list = [["shay_dayan@gmail.com","1234"],["shay_dayan@gmail.com","1234567"],["shay_dayan@gmail.co","12345"],["shay_dayan@gmail.com","1234567"],["shay_dayan@gmail.com",""],["","12345"],["",""]]
