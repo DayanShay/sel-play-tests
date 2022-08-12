dd3529e86b10fe07cbae6f97b19e860ca1258d6e
shay dayan <shay.etl.dayan@gmail.com>
2022-08-12 03:15:41 +0300

    data.py was added

--- selenium/data.py
+++ selenium/data.py
@@ -1,4 +1,4 @@
 right_user = ["shay_dayan@gmail.com","123456"]
-wrong_users_list = [["","123456"],["shay_dayan@gmail.com",""],["",""]]
+wrong_users_list = [["shay_dayan@gmail.com","1234"],["shay_dayan@gmail.com","1234567"],["shay_dayan@gmail.co","12345"],["shay_dayan@gmail.com","1234567"]]
 path_driver = r'chromedriver.exe'
 
